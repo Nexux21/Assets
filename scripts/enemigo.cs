1012f70c03950998c675a2ec7e389319ec6932d6
Nexux21 <jesusserranocosio300@gmail.com>
2026-05-25 09:35:10 -0500

    1

--- scripts/enemigo.cs
+++ scripts/enemigo.cs
@@ -1,8 +1,13 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class enemigo : MonoBehaviour
+
 {
     public GameObject target;
+    public float speed;
+    public float radiusAttack;
+    public float radiusMovement;
     void Start()
     {
 
@@ -16,7 +21,22 @@ public class enemigo : MonoBehaviour
 
     public void FollowTarget()
     {
-        Vector3 targetPosition = target.transform.position;
+        Vector3 dir = (target.transform.position - transform.position).normalized;
 
+        if (Vector3.Distance(target.transform.position, transform.position) < radiusMovement)
+        {
+            if (Vector3.Distance(target.transform.position, transform.position) < radiusAttack)
+            {
+                Debug.Log("atacando >;l ");
+
+            }
+            else
+            {
+                transform.position += dir * speed * Time.deltaTime;
+
+
+            }
+        }
     }
 }
+        
